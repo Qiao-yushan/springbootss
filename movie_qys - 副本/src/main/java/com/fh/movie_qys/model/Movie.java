d8797fd0dc3ef2f8611aa92ef9a2665cd14fdeb7
Your Name <you@example.com>
2020-08-16 09:23:25 +0800

    框架搭建

---
+++ movie_qys - 副本/src/main/java/com/fh/movie_qys/model/Movie.java
@@ -0,0 +1,7 @@
+package com.fh.movie_qys.model;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+
+
+public class Movie {
+}
---
+++ movie_qys - 副本/src/test/java/com/fh/movie_qys/MovieQysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fh.movie_qys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieQysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
